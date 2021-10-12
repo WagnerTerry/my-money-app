a07fe9375a89d5a312f493e0997ba97c5cb6b93d
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-10-11 22:29:38 -0300

    aplicando novas modalidades no app my money

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getList } from "./billingCycleActions";
+import { getList, showUpdate, showDelete } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
   componentWillMount() {
     this.props.getList();
-    console.log("s", this.props.list);
+  }
+
+  renderRows() {
+    const list = this.props.list || [];
+    return list.map((bc) => (
+      <tr key={bc._id}>
+        <td>{bc.name}</td>
+        <td>{bc.month}</td>
+        <td>{td.year}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => this.props.showUpdate(bc)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.props.showDelete(bc)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </td>
+      </tr>
+    ));
   }
   render() {
     return (
@@ -17,9 +41,10 @@ class BillingCycleList extends Component {
               <th>Nome</th>
               <th>Mês</th>
               <th>Ano</th>
+              <th className="table-actions">Ações</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>{this.renderRows()}</tbody>
         </table>
       </div>
     );
@@ -28,6 +53,5 @@ class BillingCycleList extends Component {
 
 const mapStateToProps = (state) => ({ list: state.billingCycle.list });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ getList }, dispatch);
+  bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
-// export default BillingCycleList;
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Header from "../common/template/header";
 import SideBar from "../common/template/sideBar";
 import Footer from "../common/template/footer";
+import Messages from "../common/msg/messages";
 
 export default (props) => (
   <div className="wrapper">
@@ -11,5 +12,6 @@ export default (props) => (
     <SideBar />
     <div className="content-wrapper">{props.children}</div>
     <Footer />
+    <Messages />
   </div>
 );
